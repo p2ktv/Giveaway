6899f4d4a84123480dd4b3f5e7aa7e9938cf7bef
ezzz <55191289+ezzz1337@users.noreply.github.com>
2021-01-01 02:06:06 +0100

    Take prefix from Config.config file

--- main.py
+++ main.py
@@ -2,19 +2,19 @@ import discord
 from discord.ext import commands
 from discord.ext.commands import Bot
 
-from Config.start import *
+from Config.start import * #wildcard import entire config file
 from Config.config import cogs, to_remove_commands, TOKEN
 
 
-client: Bot = commands.Bot(command_prefix=PREFIX)
+client: Bot = commands.Bot(command_prefix=PREFIX) #actual client
 
-discord.Intents().default()
+discord.Intents().default() # activation intents
 
 
 @client.event
 async def on_ready():
-    start(client, cogs, to_remove_commands)
+    start(client, cogs, to_remove_commands) #init bot -> load cogs & remove cmds
     pass
 
 
-client.run(TOKEN)
+client.run(TOKEN) #run client
